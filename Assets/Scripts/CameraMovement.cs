ee75c23cbe67d47fd8b1e2e2617f0724455d2c79
jorgenhlehne <32197885+jorgenhlehne@users.noreply.github.com>
2020-08-03 19:11:09 +0200

    Added camera rig prefab

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+// From https://catlikecoding.com/unity/tutorials/hex-map/part-5/
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private Transform swivel, stick;
+
+    private float zoom = 1f;
+
+    public float stickMinZoom, stickMaxZoom;
+
+    public float swivelMinZoom, swivelMaxZoom;
+
+    public float moveSpeedMinZoom, moveSpeedMaxZoom;
+
+    //public float rotationSpeed;
+
+    //private float rotationAngle;
+
+    private static CameraMovement instance;
+
+    public static bool Locked { set { instance.enabled = !value; } }
+
+    private void Awake()
+    {
+        instance = this;
+        swivel = transform.GetChild(0);
+        stick = swivel.GetChild(0);
+    }
+
+    private void Update()
+    {
+        float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (zoomDelta != 0f)
+        {
+            AdjustZoom(zoomDelta);
+        }
+
+        /*
+        float rotationDelta = Input.GetAxis("Rotation");
+        if (rotationDelta != 0f)
+        {
+            AdjustRotation(rotationDelta);
+        }
+        */
+
+        float xDelta = Input.GetAxis("Horizontal");
+        float zDelta = Input.GetAxis("Vertical");
+        if (xDelta != 0f || zDelta != 0f)
+        {
+            AdjustPosition(xDelta, zDelta);
+        }
+    }
+
+    //Zooms the camera
+    private void AdjustZoom(float delta)
+    {
+        zoom = Mathf.Clamp01(zoom + delta);
+
+        float distance = Mathf.Lerp(stickMinZoom, stickMaxZoom, zoom);
+        stick.localPosition = new Vector3(0f, 0f, distance);
+
+        float angle = Mathf.Lerp(swivelMinZoom, swivelMaxZoom, zoom);
+        swivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
+    }
+
+    /*
+    //Rotates the camera
+    private void AdjustRotation(float delta)
+    {
+        rotationAngle += delta * rotationSpeed * Time.deltaTime;
+        if (rotationAngle < 0f)
+        {
+            rotationAngle += 360f;
+        }
+        else if (rotationAngle >= 360f)
+        {
+            rotationAngle -= 360f;
+        }
+        transform.localRotation = Quaternion.Euler(0f, rotationAngle, 0f);
+    }
+    */
+
+    //Moves the camera
+    private void AdjustPosition(float xDelta, float zDelta)
+    {
+        Vector3 direction =
+            transform.localRotation *
+            new Vector3(xDelta, 0f, zDelta).normalized;
+        float damping = Mathf.Max(Mathf.Abs(xDelta), Mathf.Abs(zDelta));
+        float distance = Mathf.Lerp(moveSpeedMinZoom, moveSpeedMaxZoom, zoom) * damping * Time.deltaTime;
+
+        Vector3 position = transform.localPosition;
+        position += direction * distance;
+        transform.localPosition = position;
+    }
+
+    public static void ValidatePosition()
+    {
+        instance.AdjustPosition(0f, 0f);
+    }
+}
