186419151d0af63582ca24bca9a24516b1ef5cca
jorgenhlehne <32197885+jorgenhlehne@users.noreply.github.com>
2020-08-06 16:55:18 +0200

    Added idle, sinking animation. Added boat prefab with hierarchy for animations.

---
+++ Assets/SinkingTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkingTest : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Sink");
+        }
+    }
+}
